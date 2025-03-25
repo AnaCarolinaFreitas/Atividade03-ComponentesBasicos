2260d2b71c17a4e129dc59a48c203a287852c355
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-03-25 15:30:23 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
+import { StyleSheet, Text, TextInput, TouchableOpacity, Button, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Estude com o miranha</Text>
+      <Image style={styles.img} source={require('./img/SpiderManStudy.jpg')} />
+      <TextInput style={styles.input} placeholder="Qual a matéria?" />
+      <TouchableOpacity style={styles.button}>
+      <Button
+          title="Estudar" onPress={() => alert('O Homem Aranha teve que sair para uma missão, mas bons estudos!')} />
+      </TouchableOpacity>
+        
     </View>
   );
 }
@@ -16,5 +22,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  img: {
+    width: 200,
+    height: 300,
+    margin: 20,
+  },
+
+  input: {
+    width: 200,
+    height: 40,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#000',
+    margin: 10,
+  },
+
+  button: {
+    width: 200,
+    margin: 10,
   },
 });
